b97e8e581e0cd14dab1548a3f48f7a2831755930
Eric Toshio <eric.toshio.e.s@gmail.com>
2019-07-04 16:05:18 -0300

    programa inicial

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import gym
 env = gym.make('Breakout-ram-v0')
 env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(10000):
+        env.render()
+
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            print(i_episode ,"Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
